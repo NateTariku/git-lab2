95e13c2cc94cb24f5f8bc196d9b82f9f3ed837c4
Natnael Tariku <nt863818@ohio.edu>
2019-09-04 17:01:33 -0400

    @markamay

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Nate Tariku>
+ *        Date: <Sep 4 2019>
+ * Description: Program that prints out Hello World.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout<<" Hello Git!!!!"<<endl;
+
+    return 0;
+}// main
